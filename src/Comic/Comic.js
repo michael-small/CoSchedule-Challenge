9e8244e52c51069510008970811390697277800f
michael-small <small203@morris.umn.edu>
2020-07-07 13:42:13 -0500

    Create functional Comic component

---
+++ src/Comic/Comic.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Comic.css';
+
+const person = (props) => {
+    return (
+        <div className="Comic">
+            <h2>Comic title will go here</h2>
+            <p>More info on the comic, including its picture, will go below.</p>
+            <p>Here is the comic from July 7, 2020. Hardcoded in for reference before working on the API.</p>
+            <img src="https://imgs.xkcd.com/comics/universal_rating_scale.png" alt="Comic from July 7, 2020" />
+        </div>
+    )
+}
+
+export default person;
