46e3b8e9cf13f613ff251e9e897656a3bf09834b
michael-small <small203@morris.umn.edu>
2020-07-10 16:56:18 -0500

    `GET` single comics by number using server
    Includes a search bar to find comic by number, which will later be refactored into its own component. 
    
    Includes random comic button

--- src/App.js
+++ src/App.js
@@ -4,36 +4,77 @@ import ComicSearch from './ComicSearch/ComicSearch';
 import './App.css';
 
 class App extends Component {
-  state = {
-    hardcodedTestComics: [ // Before retrieving from the API, I am crafting the barebone html to display a comic and need filler
-      { 
-        month: "7",
-        num: 2329,
-        link: "",
-        year: "2020",
-        news: "",
-        safe_title: "Universal Rating Scale",
-        transcript: "",
-        alt: "There are plenty of finer gradations. I got 'critically endangered/extinct in the wild' on my exam, although the curve bumped it all the way up to 'venti.'",
-        img: "https://imgs.xkcd.com/comics/universal_rating_scale.png",
-        title: "Universal Rating Scale",
-        day: "6" 
-    }
-    ]
+
+  constructor() {
+    super();
+    this.state = {
+      comic: [],
+      comicNumber: '',
+      error: ''
+    };
   }
-  
+
+  componentDidMount() {
+    this.getComic('2330');
+  }
+
+  getComic = comicNumber => {
+    let comicUrl = '/comic/' + comicNumber;
+    this.setState({error: ''});
+    
+    fetch(comicUrl) 
+    .then(res => res.json())
+    .then(comic => this.setState({comic: comic}, () => console.log('comic fetched...', comic)))
+    .catch((error) => {
+      console.error('Error:', error);
+      this.setState({error: 'ERROR: Comic #' + comicNumber + ' not found'});
+    });
+  }  
+
+  comicSearchSubmit = (event) => {
+    this.getComic(this.state.comicNumber);
+    event.preventDefault();
+  }
+
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  getRandomComicNum(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   render () {
+    let errorBox;
+    if(this.state.error!=='') {
+      errorBox = <p className="errorMsg">{this.state.error}</p>
+    }
     return (
       <div className="App">
+      <h1>bootleg xkcd</h1>
+        <form onSubmit={this.comicSearchSubmit} className="comicSearch"> {/* TODO: Break out into own component */}
+          <input 
+            type="text" 
+            name="comicNumber" 
+            placeholder="Seach by comic number" 
+            value={this.state.comicNumber}
+            onChange={this.onChange}
+          />
+          <input 
+            type="submit" 
+            value="Submit" 
+            className="btn"
+          />
+        </form>
+        <button onClick={() => this.getComic(this.getRandomComicNum(2330))}>Random</button>
+        {errorBox}
         <Comic 
-          title={this.state.hardcodedTestComics[0].title}
-          img={this.state.hardcodedTestComics[0].img}
-          alt={this.state.hardcodedTestComics[0].alt}
-          month={this.state.hardcodedTestComics[0].month}
-          day={this.state.hardcodedTestComics[0].day}
-          year={this.state.hardcodedTestComics[0].year}
-          num={this.state.hardcodedTestComics[0].num}/>
-        <ComicSearch></ComicSearch>
+          img= {this.state.comic.img}
+          alt= {this.state.comic.alt}
+          title= {this.state.comic.title}
+          num= {this.state.comic.num}
+          month= {this.state.comic.month}
+          day= {this.state.comic.day}
+          year= {this.state.comic.year}
+        />
       </div>
     );
   }
