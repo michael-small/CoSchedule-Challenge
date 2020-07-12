b0ba8c7358652ea3bc461eabce527079449699ac
michael-small <small203@morris.umn.edu>
2020-07-12 18:39:44 -0500

    Move styling of  to CSS file

--- src/CommentArea/CommentArea.js
+++ src/CommentArea/CommentArea.js
@@ -4,23 +4,18 @@ import './CommentArea.css';
 class CommentArea extends Component {
 
     render () {
-        const myStyle = {
-            'text-align': 'left',
-            'padding-left': '1rem'
-        }
-
         return (
             <div style={{'margin-bottom': '1rem'}}>
                 <h2>Comments</h2>
-                <div className="Comic">
-                <div style={myStyle}>
+                <div className="commentArea">
+                <div className="comment">
                     <p>Michael Small</p>
                     <p>Sep 7, 2021 3:46am</p>
                     <p>The meat of the comment. I really aught to install a lorem ipsum plugin for VSC, because it was 
                     very annoying to write this out for myself. Oh well. I guess I aught to write out just a bit more so I can see it
                     wrap onto a third line.</p>
                 </div>            
-                <div style={myStyle}>
+                <div className="comment">
                     <p>Michael Small</p>
                     <p>Sep 7, 2021 3:46am</p>
                     <p >The meat of the comment. I really aught to install a lorem ipsum plugin for VSC, because it was 
