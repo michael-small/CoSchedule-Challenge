2640edbfa29485c1bf0c84e8536848d201b95688
michael-small <small203@morris.umn.edu>
2020-07-12 18:42:06 -0500

    Style/seperate individual comments
    All comments are top-level: no subcomments.

--- src/CommentArea/CommentArea.js
+++ src/CommentArea/CommentArea.js
@@ -8,20 +8,20 @@ class CommentArea extends Component {
             <div style={{'margin-bottom': '1rem'}}>
                 <h2>Comments</h2>
                 <div className="commentArea">
-                <div className="comment">
-                    <p>Michael Small</p>
-                    <p>Sep 7, 2021 3:46am</p>
-                    <p>The meat of the comment. I really aught to install a lorem ipsum plugin for VSC, because it was 
-                    very annoying to write this out for myself. Oh well. I guess I aught to write out just a bit more so I can see it
-                    wrap onto a third line.</p>
-                </div>            
-                <div className="comment">
-                    <p>Michael Small</p>
-                    <p>Sep 7, 2021 3:46am</p>
-                    <p >The meat of the comment. I really aught to install a lorem ipsum plugin for VSC, because it was 
-                    very annoying to write this out for myself. Oh well. I guess I aught to write out just a bit more so I can see it
-                    wrap onto a third line.</p>
-                </div>
+                    <div className="comment">
+                        <p>Michael Small</p>
+                        <p>Sep 7, 2021 3:46am</p>
+                        <p>The meat of the comment. I really aught to install a lorem ipsum plugin for VSC, because it was 
+                        very annoying to write this out for myself. Oh well. I guess I aught to write out just a bit more so I can see it
+                        wrap onto a third line.</p>
+                    </div>            
+                    <div className="comment">
+                        <p>Michael Small</p>
+                        <p>Sep 7, 2021 3:46am</p>
+                        <p >The meat of the comment. I really aught to install a lorem ipsum plugin for VSC, because it was 
+                        very annoying to write this out for myself. Oh well. I guess I aught to write out just a bit more so I can see it
+                        wrap onto a third line.</p>
+                    </div>
                 </div>
             </div>
         )
