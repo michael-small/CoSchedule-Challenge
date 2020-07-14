b3eaabcb06242364a105cf14644dfc3bf6b3ee4f
michael-small <small203@morris.umn.edu>
2020-07-13 19:53:34 -0500

    Change  from functional to class component

--- src/Comic/Comic.js
+++ src/Comic/Comic.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Comic.css';
 
-const comic = (props) => {
+export class Comic extends Component {
 
-    return (
-        <div className="Comic">
-            <h2>Comic #{props.num} ({props.month}/{props.day}/{props.year})</h2>
-            <h1>{props.title}</h1>
-            <img 
-                src={props.img} 
-                alt={props.alt} 
-                title={props.alt}/> {/* Randall Munroe uses the alt text as the text that shows on hover, but `title` is the <img> property to do that */}
-            <br/>
-            <p>Link to comic: <a href={'https://xkcd.com/' + props.num +'/'}>{'https://xkcd.com/' + props.num +'/'}</a></p>
-        </div>
-    )
+    render () {
+        return (
+            <div className="Comic">
+                <h2>Comic #{this.props.num} ({this.props.month}/{this.props.day}/{this.props.year})</h2>
+                <h1>{this.props.title}</h1>
+                <img 
+                    src={this.props.img} 
+                    alt={this.props.alt} 
+                    title={this.props.alt}/> {/* Randall Munroe uses the alt text as the text that shows on hover, but `title` is the <img> property to do that */}
+                <br/>
+                <p>Link to comic: <a href={'https://xkcd.com/' + this.props.num +'/'}>{'https://xkcd.com/' + this.props.num +'/'}</a></p>
+            </div>
+        )
+    }
+
 }
 
-export default comic;
+export default Comic;
